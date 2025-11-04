46295eba6de7292e6eccc50787253bd50f7b6daf
roskyb <jorge.blanco.gil.00@gmail.com>
2025-11-04 23:23:57 +0100

    fix: docker file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import { defineConfig, passthroughImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import { BASE_URL } from "./src/consts.ts";
 import { DEFAULT_LOCALE_SETTING, LOCALES_SETTING } from "./src/i18n/locales";
 import tailwindcss from "@tailwindcss/vite";
